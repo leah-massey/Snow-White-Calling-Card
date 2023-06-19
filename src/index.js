4a54d3cb4c9cdf891562a155a4a879141a9d29e1
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-06-19 15:54:34 +0100

    Avatar, Skillset and Intro components created.

--- src/index.js
+++ src/index.js
@@ -13,6 +13,18 @@ function App() {
   );
 }
 
+function Avatar() {
+  return <img src="snow-white.jpg" />;
+}
+
+function Intro() {
+  return <h1>My name is Snow White</h1>;
+}
+
+function Skillset() {
+  return <p>These are my skills</p>;
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
